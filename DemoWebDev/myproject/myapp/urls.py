267713735a7a86397cbb5f7bec02e88363e8aeaa
Christian Yang <93405388+tianchrisy@users.noreply.github.com>
2025-02-16 22:08:56 -0500

    Created a database and configured the admin portal

--- DemoWebDev/myproject/myapp/urls.py
+++ DemoWebDev/myproject/myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home 
+from . import views
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', views.home, name='home'),
+    path("todos/", views.todos, name='todos'),
 ]
